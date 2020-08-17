bd7d245b2b70124795a43f4efe0b57c989202a31
Alfonso-270702 <tebaksiapa27@gmail.com>
2020-08-18 00:36:55 +0700

    perbaikin typo

--- server/series/controller/tvSeries.js
+++ server/series/controller/tvSeries.js
@@ -32,7 +32,7 @@ class TvSeriesController {
       tags,
     })
       .then((data) => {
-        res.status(200).json({ msg: "Successfully edit movie" });
+        res.status(200).json({ msg: "Successfully edit series" });
       })
       .catch((err) => {
         res.status(500).json({ msg: "Internal Server Error" });
@@ -42,7 +42,7 @@ class TvSeriesController {
   static deleteTvSeries(req, res, next) {
     TvSeries.delete(req.params.id)
       .then((data) => {
-        res.status(200).json({ msg: "Successfully remove movie" });
+        res.status(200).json({ msg: "Successfully remove series" });
       })
       .catch((err) => {
         res.status(500).json({ msg: "Internal Server Error" });
--- server/series/route/index.js
+++ server/series/route/index.js
@@ -3,7 +3,7 @@ const TvSeriesController = require("../controller/tvSeries");
 
 route.get("/tv-series", TvSeriesController.findTvSeries);
 route.post("/tv-series", TvSeriesController.addTvSeries);
-route.put("/tv-series", TvSeriesController.updatedTvSeries);
-route.delete("/tv-series", TvSeriesController.deleteTvSeries);
+route.put("/tv-series/:id", TvSeriesController.updatedTvSeries);
+route.delete("/tv-series/:id", TvSeriesController.deleteTvSeries);
 
 module.exports = route;
